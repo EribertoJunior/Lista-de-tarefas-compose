0e5c228629da8987870cd7bee1cecc547dab2a3e
Eriberto Junior <eriberto.brasileiro@gmail.com>
2023-03-09 11:16:06 -0300

    adicao de testes em ListRepositoryImplTest.kt

--- app/src/test/java/com/example/listacompose/data/repository/ListRepositoryImplTest.kt
+++ app/src/test/java/com/example/listacompose/data/repository/ListRepositoryImplTest.kt
@@ -1,30 +1,78 @@
 package com.example.listacompose.data.repository
 
 import com.example.listacompose.data.dataSource.ListDataSource
+import com.example.listacompose.data.model.Task
+import com.example.listacompose.domain.repository.ListRepository
 import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.spyk
 import junit.framework.TestCase.assertEquals
-import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
 
 
 internal class ListRepositoryImplTest {
 
-    private val listDataSourceMock: ListDataSource = mockk()
-    private val listRepository = spyk(ListRepositoryImpl(listDataSource = listDataSourceMock))
+    private lateinit var listDataSourceMock: ListDataSource
+    private lateinit var listRepository: ListRepository
+
+    @Before
+    fun before() {
+        listDataSourceMock = mockk()
+        listRepository = spyk(ListRepositoryImpl(listDataSource = listDataSourceMock))
+    }
 
     @Test
-    fun `quando houver itens salvos deve retornar esses itens`() {
+    fun `quando houver um item salvo deve retornar um item`() {
+
         coEvery { listDataSourceMock.getTasks() } returns flow {
-            emit(listOf(mockk(relaxed = true)))
+            emit(
+                listOf(mockk(relaxed = true))
+            )
         }
 
         runBlocking {
             val result = listRepository.getList()
-            assertEquals(1, result.count())
+            assertEquals(1, result.first().count())
+        }
+    }
+
+    @Test
+    fun `quando houver cinco itens salvos, deve retornar cinco itens`() {
+        coEvery { listDataSourceMock.getTasks() } returns flow {
+            emit(
+                listOf(
+                    mockk(relaxed = true),
+                    mockk(relaxed = true),
+                    mockk(relaxed = true),
+                    mockk(relaxed = true),
+                    mockk(relaxed = true)
+                )
+            )
+        }
+
+        runBlocking {
+            val result = listRepository.getList()
+            assertEquals(5, result.first().count())
+        }
+    }
+
+    @Test
+    fun `deve retornar o mesmo objeto que veio do 'data source'`(){
+        val task = Task(0, "teste", "lala")
+
+        coEvery { listDataSourceMock.getTasks() } returns flow {
+            emit(
+                listOf(task)
+            )
+        }
+
+        runBlocking {
+            val listFlow = listRepository.getList()
+            assertEquals(task, listFlow.first()[0])
         }
     }
 }
